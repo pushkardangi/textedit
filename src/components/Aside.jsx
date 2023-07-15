c7a2f1410c3bf5e0c9ee608c86990b4288e7387b
Pushkar Dangi <pushkardangi746@gmail.com>
2023-07-15 16:23:33 +0530

    ui: reponsive aside, hidden btn text for mobile, scroll features tab

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -13,10 +13,9 @@ function Aside() {
   { (theme === "dark") ? (UI = darkStyles) : (UI = lightStyles) }
 
   return (
-    <aside className={`w-1/5 py-2 overflow-auto ${UI.textColor}`}>
-      {/* h-full kar sakte h niche vale div ki, scroll lane k liye */}
-      <div className={`rounded-lg h-full shadow-lg ${UI.sectionColor} ${UI.border} ${UI.borderColor}`}>
-        <div className={`p-3 border-b-2 text-lg ${theme == "light" ? "border-[#e5e7eb]" : UI.borderColor}`}>
+    <aside className={`md:w-1/5 py-2 ${UI.textColor}`}>
+      <div className={`rounded-lg shadow-lg h-2/4 sm:h-full overflow-auto ${UI.sectionColor} ${UI.border} ${UI.borderColor}`}>
+        <div className={`p-3 border-b-2 text-lg hidden md:block ${theme == "light" ? "border-[#e5e7eb]" : UI.borderColor}`}>
           Features
         </div>
 
@@ -30,8 +29,8 @@ function Aside() {
               <div className="logo">
                 <img className="w-8 rounded" src={feature.image} alt={feature.title} />
               </div>
-              <div className="mx-2 text-base">{feature.title}</div>
-              <div className="ml-auto">
+              <div className="mx-2 text-base hidden md:block">{feature.title}</div>
+              <div className="ml-auto hidden md:block">
                 <img className="w-2.5 mr-1" src={dragBtn} alt="drag-btn" />
               </div>
             </div>
