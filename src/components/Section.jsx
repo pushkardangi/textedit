d09ebdf0333d2f0c399cc468f931217491485858
Pushkar Dangi <pushkardangi746@gmail.com>
2023-07-18 18:32:58 +0530

    ui: textarea row count depends on number of newlines in text

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -8,6 +8,11 @@ function Section() {
   const text = useText();
   const textUpdate = useTextUpdate();
 
+  const getRowCount = (text) => {
+    const lines = text.split('\n');
+    return lines.length + 19;
+  };
+
   return (
     <section className="flex flex-col flex-1 px-4 md:px-20">
       {/* Toolbar */}
@@ -20,9 +25,10 @@ function Section() {
       {/* Textarea */}
       <div className="overflow-auto">
         <textarea id="textBox"
-          className={`w-full p-4 h-[90rem] text-lg ${UI.textColor} ${UI.bgColor}`}
+          className={`w-full p-4 text-lg ${UI.textColor} ${UI.bgColor}`}
           placeholder="Start typing or paste text here"
           value={text}
+          rows={getRowCount(text)}
           onChange={(e) => { textUpdate(e.target.value) }}
           >
         </textarea>
